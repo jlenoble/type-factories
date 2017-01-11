fb4387bda3f21131a7cc4bf6c07cf947417e192b
Jason Lenoble <jason.lenoble@gmail.com>
2017-01-11 14:30:32 +0100

    Added implicit conversions to UID types

--- src/uid.js
+++ src/uid.js
@@ -20,5 +20,5 @@ export default function UIDType (prefix) {
     },
   };
 
-  return ValueType(prototype, descriptors);
+  return ValueType(prototype, descriptors, _value);
 };
--- test/uid.test.js
+++ test/uid.test.js
@@ -2,45 +2,60 @@ import UIDType from '../src/uid';
 import {expect} from 'chai';
 
 describe('UIDType', function () {
-  describe("generates Types that create objects whose properties 'value'", function () {
-    it("always increment, starting at 1", function () {
-      var UID = UIDType();
+  describe('generates Types that create objects', function () {
+    describe(`whose properties 'value'`,
+    function () {
+      it('always increment, starting at 1', function () {
+        const UID = UIDType();
 
-      expect(UID().value).to.equal(1);
-      expect(UID().value).to.equal(2);
-      expect(UID().value).to.equal(3);
-      expect(UID().value).to.equal(4);
+        expect(UID().value).to.equal(1);
+        expect(UID().value).to.equal(2);
+        expect(UID().value).to.equal(3);
+        expect(UID().value).to.equal(4);
+      });
+
+      it('increment independently', function () {
+        const UID1 = UIDType();
+        const UID2 = UIDType();
+
+        expect(UID1().value).to.equal(1);
+        expect(UID2().value).to.equal(1);
+        expect(UID2().value).to.equal(2);
+        expect(UID2().value).to.equal(3);
+        expect(UID1().value).to.equal(2);
+      });
+
+      it('cannot be modified', function () {
+        const UID = UIDType();
+
+        const uid = UID();
+        expect(uid.value).to.equal(1);
+        expect(function () {
+          uid.value = 27;
+        }).to.throw(TypeError, /set.*property.*has only a getter/);
+        expect(uid.value).to.equal(1);
+      });
+
+      it('can have a prefix and still increment properly', function () {
+        const UID = UIDType('foobar');
+
+        expect(UID().value).to.equal('foobar1');
+        expect(UID().value).to.equal('foobar2');
+        expect(UID().value).to.equal('foobar3');
+        expect(UID().value).to.equal('foobar4');
+      });
     });
 
-    it("increment independently", function () {
-      var UID1 = UIDType();
-      var UID2 = UIDType();
+    describe('that can be converted implicitly to base to int or string',
+    function () {
+      const UID1 = UIDType();
+      const UID2 = UIDType('id');
 
-      expect(UID1().value).to.equal(1);
-      expect(UID2().value).to.equal(1);
-      expect(UID2().value).to.equal(2);
-      expect(UID2().value).to.equal(3);
-      expect(UID1().value).to.equal(2);
-    });
+      const uid1 = UID1();
+      expect(+uid1).to.equal(1);
 
-    it("cannot be modified", function () {
-      var UID = UIDType();
-
-      var uid = UID();
-      expect(uid.value).to.equal(1);
-      expect(function () {
-        uid.value = 27;
-      }).to.throw(TypeError, /set.*property.*has only a getter/);
-      expect(uid.value).to.equal(1);
-    });
-
-    it("can have a prefix and still increment properly", function () {
-      var UID = UIDType("foobar");
-
-      expect(UID().value).to.equal("foobar1");
-      expect(UID().value).to.equal("foobar2");
-      expect(UID().value).to.equal("foobar3");
-      expect(UID().value).to.equal("foobar4");
+      const uid2 = UID2();
+      expect(uid2 + ' is 1').to.equal('id1 is 1');
     });
   });
 });
