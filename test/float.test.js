fc64669961fa1f9956f917d8d0c542bb277b1811
Jason Lenoble <jason.lenoble@gmail.com>
2017-01-21 14:28:06 +0100

    Tested api for Float type

--- test/float.test.js
+++ test/float.test.js
@@ -64,4 +64,67 @@ describe('Testing Float', function () {
     this.execBad(this.set, function () {});
     this.execBad(this.set, {});
   });
+
+  it(`Object can't be extended`, function () {
+    this.execThrow(function () {
+      const d = new Float(10);
+      d.foo = 'bar';
+    });
+
+    this.execThrow(function () {
+      const d = new Float(10);
+      Object.defineProperty(d, 'foo', {value: 'bar'});
+    });
+  });
+
+  it(`Property value can't be deleted`, function () {
+    this.execThrow(function () {
+      const d = new Float(10);
+      delete d.value;
+    });
+  });
+
+  it(`Prototype can't be extended`, function () {
+    this.execThrow(function () {
+      Float.prototype.foo = 'bar';
+    });
+  });
+
+  it(`Prototype properties can't be deleted`, function () {
+    this.execThrow(function () {
+      delete Float.prototype.toFixed;
+    });
+  });
+
+  it(`Methods mirror Number methods`, function () {
+    Object.getOwnPropertyNames(Number.prototype).filter(name =>
+      name !== 'constructor').forEach(name => {
+        const d = new Float(123.456e-7);
+
+        switch(name) {
+        case 'toExponential': case 'toFixed': case 'toPrecision':
+          expect(d[name](3)).to.equal(d.value[name](3));
+          break;
+
+        default:
+          expect(d[name]()).to.equal(d.value[name]());
+        }
+      });
+  });
+
+  it('Implicit conversion is Ok', function () {
+    const a = new Float(123.4);
+    const b = new Float(-.5);
+    const c = new Float(NaN);
+
+    expect(a + b).to.equal(122.9);
+    expect(a - b).to.equal(123.9);
+    expect(a * b).to.equal(-61.7);
+    expect(a / b).to.equal(-246.8);
+
+    expect(a + c).to.be.NaN;
+    expect(a - c).to.be.NaN;
+    expect(a * c).to.be.NaN;
+    expect(a / c).to.be.NaN;
+  });
 });
